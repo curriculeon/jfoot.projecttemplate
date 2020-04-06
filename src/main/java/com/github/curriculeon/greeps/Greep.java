31992c2351a2f19c9af62c01f8b0e2892867f020
leon hunter <xleonhunter@gmail.com>
2020-04-06 11:37:41 -0400

    updated template

--- src/main/java/com/github/curriculeon/greeps/Greep.java
+++ src/main/java/com/github/curriculeon/greeps/Greep.java
@@ -1,7 +1,7 @@
 package com.github.curriculeon.greeps;
 
 import com.github.git_leon.RandomUtils;
-import com.github.git_leon.jfoot.sprite.SpriteSensorDecorator;
+import com.github.git_leon.jfoot.system.resources.JFootImage;
 import greenfoot.Actor;
 
 /**
@@ -10,149 +10,22 @@ import greenfoot.Actor;
  * @author (your name here)
  * @version 0.1
  */
-public class Greep extends Creature {
-    /**
-     * Create a creature at its ship.
-     *
-     * @param ship
-     */
-    public Greep(Spaceship ship) {
-        super(ship);
-        setImage(getCurrentImage());
+public class Greep extends Actor {
+    public Greep() {
+        setImage(new JFootImage(new StringBuilder()
+                .append(System.getProperty("user.dir"))
+                .append("/src/main/resources/images/greeps/greep.png")
+                .toString()));
     }
 
-    @Override
-    protected void behave() {
-        if (isCarryingTomato()) {
-            if (isAtShip()) {
-                dropTomato();
-            } else {
-                turnTowardsHome();
-            }
+    public void act() {
+        move(5);
+        if(isAtEdge()) {
+            turn(30);
         }
-        move();
-    }
 
-    private Boolean isToLeft(Actor actor) {
-        int currentRotation = getRotation();
-        turnTowards(actor);
-        int relativeAngle = getRotation();
-        if (relativeAngle > 180)
-            return true;
-        setRotation(currentRotation);
-        return false;
-    }
-
-    private GreepSpit getIntersectingSpit() {
-        return (GreepSpit) getOneIntersectingObject(GreepSpit.class);
-    }
-
-    private boolean isAtSpit(String colorName) {
-        GreepSpit potentialSpit = (GreepSpit) getOneIntersectingObject(GreepSpit.class);
-        if (potentialSpit != null) {
-            return potentialSpit.getColor().equalsIgnoreCase(colorName);
+        if(RandomUtils.createBoolean(10)) {
+            turn(RandomUtils.createInteger(-90, 90));
         }
-        return false;
-    }
-
-    public Boolean isWaitingForAssistance() {
-        return isAtTomatoes() && !isCarryingTomato();
-    }
-
-
-    public Boolean isWaitingToAssist() {
-        if (isAtTomatoes()) {
-            for (Greep greep : getSurroundingTomatoPile().getIntersectingObjects(Greep.class)) {
-                if (!greep.isCarryingTomato()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-    public void waitForTomatoLoadingAssistance() {
-        turnTowards(getSurroundingTomatoPile());
-        move();
-        loadTomato();
-    }
-
-
-    public Boolean isReturningToShip() {
-        return isCarryingTomato();
-    }
-
-
-    public void returnToShip() {
-        turnTowardsHome(3);
-        move();
-    }
-
-
-    public Boolean shouldSeekTomatoPile() {
-        return !isCarryingTomato();
-    }
-
-
-    public void seekTomatoPile() {
-        move();
-    }
-
-
-    public void turnRandomDegrees() {
-        turnRandomDegrees(15, 90);
-    }
-
-
-    public void turnRandomDegrees(int minimumTurn, int maximumTurn) {
-        turn(RandomUtils.createInteger(minimumTurn, maximumTurn));
-    }
-
-
-    public void turnRandomly(int minimumTurn, int maximumTurn, float likelihoodOfTurn) {
-        if (minimumTurn > maximumTurn) {
-            Integer temp;
-            temp = maximumTurn;
-            maximumTurn = minimumTurn;
-            minimumTurn = temp;
-        }
-        if (RandomUtils.createBoolean(likelihoodOfTurn)) {
-            turnRandomDegrees(minimumTurn, maximumTurn);
-        }
-    }
-
-    /**
-     * Is there any food here where we are? If so, try to load some!
-     */
-
-    public void checkFood() {
-        // check whether there's a tomato pile here
-        if (isAtTomatoes()) {
-            loadTomato();
-            // Note: this attempts to load a tomato onto *another* Greep. It won't
-            // do anything if we are alone here.
-        }
-    }
-
-
-    /**
-     * This method specifies the image we want displayed at any time. (No need
-     * to change this for the competition.)
-     */
-
-    public String getCurrentImage() {
-        if (isCarryingTomato())
-            return "greep-with-food.png";
-        else
-            return "greep.png";
-    }
-
-    /**
-     * Create a Greep with its home space ship.
-     */
-
-    public static String getAuthorName() {
-        return "Anonymous";
     }
 }
